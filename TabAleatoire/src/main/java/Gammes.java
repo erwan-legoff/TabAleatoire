1f8fa3e3f07106fce02417035adaa5d3ee7c8498
erwil <rw4nit0@gmail.com>
2020-02-20 14:21:45 +0100

    ajout de commentaires détaillés dans Gammes

--- TabAleatoire/src/main/java/Gammes.java
+++ TabAleatoire/src/main/java/Gammes.java
@@ -1,8 +1,21 @@
 import java.util.ArrayList;
 
 public class Gammes {
+    //Classe qui permet de faire respecter la gamme
+    // la note de la corde permet de décaler la gamme pour que les cases correspondent avec les notes
+        /*
+        EXEMPLE :
+        pour la corde LA (A), la note SI (B) est à la case 2 (LA, LA#, <SI>)
+        une case = un demi ton = la différence entre LA et LA#
+        donc la note SI est numérotée 2 pour la corde LA
+        par contre, pour la corde SI, elle est notée 0 etc
+        On se sert de la note de la corde pour pouvoir décaler les cases
+        ATTENTION : il n'y a pas de MI# et de SI#
+        donc entre MI et FA il y a un demi ton ou une case
+        pareil entre SI et DO
+         */
 
-
+                                                //DO      DO#  RE    RE#    MI     FA     FA#   SOL    SOL#  LA    LA#   SI
     static final String[] notes =                {"C",   "C#", "D", "D#",   "E",   "F",  "F#",  "G",  "G#", "A",   "A#", "B"};    //liste les notes
 
     private static final boolean[] chromatique = {true, true,  true, true,  true, true, true,  true, true,  true, true,  true};//on stocke les gammes
@@ -11,19 +24,31 @@ public class Gammes {
                                                                         //pour pouvoir les choisir
 
 
-    static ArrayList<Integer> boolVersListeCase(int note_corde, int tonalite, boolean[] gamme, int case_min, int case_max) {//convertit un tableau
-                                                                                            // de bool en cases
+    static ArrayList<Integer> boolVersListeCase(int note_corde, int tonalite, boolean[] gamme, int case_min, int case_max) {
+        //convertit un tableau de bool en cases
+        //permet de ne regrouper que les notes qui appartiennent à la gamme
+        //cela facilite la génération aléatoire
+        //comparée à l'utilisation d'un tableau booléen
+        //mais le tableau booléen est plus facile à utiliser pour rentrer une gamme
+        //cela permet donc de faciliter l'écriture des gammes
+        //tout en facilitant le code
+
         ArrayList<Integer> gammeCase = new ArrayList<>(); //liste qu'on va retourner, convertie en numéro de case
         int cases = 0;
 
-        while (cases-12+note_corde+tonalite<case_max)
+        while (cases-12+note_corde+tonalite<case_max) //dépasse la case 12 pour commencer par la première note
+                                                    //de la corde qui correspond à la gamme
+                                                        // permet d'avoir toutes les notes du tableau bien répartie
         {
             if ((cases + note_corde+tonalite) >= 12+case_min)
             {
                 if (gamme[cases % 12])//ajoute la note si elle est présente dans la gamme
+                    //on prend modulo 12 car la note est la même à l'octave au-dessus, juste plus aigue
+                    //un octave=12 demi tons = 12 cases
                 {
-
-                    gammeCase.add((cases + note_corde+tonalite) % 12);
+                    gammeCase.add((cases + note_corde+tonalite) % 12); //ajoute la case jusqu'à 11
+                                                                        // plus tard 23, plus proche du nombre total
+                                                                        //de case d'une guitare
                 }
             }
             cases++;
@@ -35,6 +60,7 @@ public class Gammes {
 
     static ArrayList<Integer> getGammeEnCase(int note_corde, int tonalite, int type_gamme, int case_min, int case_max)
     {   //Permet de renvoyer une gamme choisie dans la tonalité choisie avec l'étendue choisie
+
         return boolVersListeCase(note_corde, tonalite, tabGammes[type_gamme], case_min, case_max);
     }
 
