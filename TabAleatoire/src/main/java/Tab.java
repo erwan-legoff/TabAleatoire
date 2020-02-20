1d52738d6f48b39830c1ae896febe5a846ea035b
erwil <rw4nit0@gmail.com>
2020-02-20 13:52:57 +0100

    ajout de commentaires à la classe Tab

--- TabAleatoire/src/main/java/Tab.java
+++ TabAleatoire/src/main/java/Tab.java
@@ -12,7 +12,7 @@ public class Tab {
     private String gamme="tout";
     private Corde[] tab_corde;
 
-    public Tab(int nbCorde, int nb_temps)
+    public Tab(int nbCorde, int nb_temps) //création d'une tablature à remplir, en fonction de la taille demandée
     {
         this.nb_corde=nbCorde;
         this.nb_temps=nb_temps;
@@ -20,7 +20,7 @@ public class Tab {
         System.out.println("il y a "+ nb_temps+" temps" );
         this.tab_corde=new Corde[this.nb_corde];
         for (int i = 0; i < nb_corde ; i++) {//initialise la tablature à vide
-            tab_corde[i]=new Corde(i+1,accordage);
+            tab_corde[i]=new Corde(i+1,accordage);//création de corde vide
         }
 
         //initialisation de dejaUtilise
@@ -90,48 +90,62 @@ public class Tab {
         }
     }
 
- */
+ *///ancienne fonction random, ne prenant pas en compte les gammes...
 
 
-
+    //Fonction centrale : permet de générer une tabature aléatoir à partir d'une gamme donnée
     public void randomGamme(int case_min, int case_max, int tonalite, int num_gamme)
     {
-        for (int num_corde_actuelle = 0; num_corde_actuelle < nb_corde; num_corde_actuelle++)
+        for (int num_corde_actuelle = 0; num_corde_actuelle < nb_corde; num_corde_actuelle++)//parcours les cordes
         {
-            double proba_silence_corde = getProba_silence_corde(num_corde_actuelle);
-
-            Corde corde_actuelle = tab_corde[num_corde_actuelle];
-            for (int temps_actuel = 0; temps_actuel < nb_temps; temps_actuel++)
+            double proba_silence_corde = getProba_silence_corde(num_corde_actuelle);//détermine la proba de silence
+                                                                                    // pour cette corde
+            Corde corde_actuelle = tab_corde[num_corde_actuelle]; //on définit la corde sur laquelle on travaille
+            for (int temps_actuel = 0; temps_actuel < nb_temps; temps_actuel++) //parcours tous les temps de cette corde
             {
                 if(proba_silence_corde>randomMinMax(0,100)||(dejaUtilise[temps_actuel] && estMelodie))
-                    corde_actuelle.addNoteFin("");
+                    corde_actuelle.addNoteFin(""); //ajoute un silence aléatoirement ou si déjà utilisée
+                                                    // et qu'on veut bien une mélodie
                 else
-                    {
+                    {   //sinon on tire au sort une note à ajouter
+                        //appel de la fonction noteRandom
                         int note_random = noteRandom(corde_actuelle, tonalite, case_min, case_max + 1, num_gamme);
+                        //Ajout de la note à la fin de la corde
                         corde_actuelle.addNoteFin(note_random);
+                        //En cas de mélodie, on ne pourra plus utiliser ce temps sur aucune des cordes pour mettre une note
                         dejaUtilise[temps_actuel]=true;
                     }
             }
-
         }
     }
+
+    //permet de changer la probabilité d'avoir un silence en fonction de la corde
+    //n'est pas parfait, mais permet de mieux répartir les notes sur toutes les cordes
     private double getProba_silence_corde(int i_corde) {
         return (45*proba_silence*nb_corde)/(i_corde+1);
     }
 
-    private static int randomMinMax(int case_min, int case_max) {
-        return (int)(Math.random() * ((case_max - (case_min)) + 1));
-    }
 
+    //permet de générer une note aléatoire dans la gamme et corde choisie
     private int noteRandom(Corde corde, int tonalite, int case_min, int case_max, int num_gamme)
     {
+        //crée un tableau avec toutes les notes de la gammes dans l'intervalle choisie
         ArrayList<Integer> cases_gammes = Gammes.getGammeEnCase(corde.getNoteCorde(),tonalite,num_gamme,case_min, case_max);
+        //sélectionne une case au hasard dans ce tableau
+        //la note sera donc oblgatoirement dans la gamme
         int numero_note_random = randomMinMax(0,cases_gammes.size()-1);
         return (int) cases_gammes.get(numero_note_random);
     }
 
+    //fonction random utilisée
+    //dans le futur elle permettra de générer un identifiant
+    //pour retrouver la même tablature random
+    private static int randomMinMax(int case_min, int case_max) {
+        return (int)(Math.random() * ((case_max - (case_min)) + 1));
+    }
 
-    public void genererGamme(int case_min, int case_max, int tonalite, int num_gamme)
+    public void genererGamme(int case_min, int case_max, int tonalite, int num_gamme)//pas vraiment utile en l'état,
+    // génère une tablature avec toutes les notes d'une gammes
     {
         for (int num_corde = 1; num_corde < nb_corde; num_corde++) { //initialisation d'une corde
             Corde corde_actuelle=new Corde(num_corde, accordage);
