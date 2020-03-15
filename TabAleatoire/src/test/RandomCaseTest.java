15cae9f84f59f644b44d1603e41c3908514ccada
erwil <rw4nit0@gmail.com>
2020-03-15 12:18:37 +0100

    test frequence randomCase OK

--- TabAleatoire/src/test/RandomCaseTest.java
+++ TabAleatoire/src/test/RandomCaseTest.java
@@ -1,8 +1,10 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.Random;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class RandomCaseTest {
@@ -57,5 +59,23 @@ class RandomCaseTest {
             }
         }
         assertTrue(test);
+    }
+    @Test
+    void test_frequence() {
+        int nbTest = 1000000;
+        boolean test=false;
+        int[] somme=new int[13];
+        float[] frequence=new float[13];
+        for (int i = 0; i <nbTest ; i++) {
+            int caseRandom= randomCase.getCaseRandom(corde);
+
+            somme[caseRandom]++;
+        }
+        for (int i = 0; i <13 ; i++) {
+            frequence[i]=100*(((float)somme[i])/nbTest);
+
+        }
+        System.out.println("frequence="+Arrays.toString(frequence));
+        assertEquals(frequence[1],100*(1/13.0),0.004);
     }
 }
